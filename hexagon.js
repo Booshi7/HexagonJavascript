68224c2068893087f7a7484281672abd386594df
Booshi7 <booshigd@gmail.com>
2024-07-20 13:11:27 +0200

    save o cas ou

--- hexagon.js
+++ hexagon.js
@@ -3,8 +3,8 @@ let boardh = 690;
 let boardw = 690;
 let context;
 
-let arrowh = 20;
-let arroww = 20;
+let arrowh = 500;
+let arroww = 500;
 let arrowrot = -Math.PI / 2;
 let arrowimg;
 
@@ -16,6 +16,12 @@ let arrow = {
 let formehexagonale = {
     width: 150,
     height: 150,
+    img: null,
+}
+
+let bg = {
+    width: 1000,
+    height: 1000,
     img: null,
 }
 
@@ -35,6 +41,7 @@ let lastFrameTime = 0;
 let fps = 0;
 let fpsCounter;
 
+//Touches
 window.addEventListener('keydown', function(e) {
     keys[e.key] = true;
 });
@@ -43,6 +50,7 @@ window.addEventListener('keyup', function(e) {
     keys[e.key] = false;
 });
 
+//fonction de départ (je crois)
 window.onload = function() {
     board = document.getElementById("board");
     board.height = boardh;
@@ -50,24 +58,23 @@ window.onload = function() {
     context = board.getContext("2d");
 
     arrowimg = document.getElementById("trianglerotated");
-    arrowimg.onload = function() {
-        requestAnimationFrame(update);
-    }
-
+    arrowimg.style.filter = "grayscale(100%)"
     formehexagonale.img = document.getElementById("hexagonal")
-    formehexagonale.onload = function() {
-        requestAnimationFrame(update)
-    }
+
+    bg.img = document.getElementById("bg");
 
 
     fpsCounter = document.getElementById("fpsCounter");
 
+    drawbg()
     drawhexagonal();
     drawArrow();
     requestAnimationFrame(update);
 }
 
+//Boucle (je crois)
 function update(timestamp) {
+    // truc de fps
     if (lastFrameTime) {
         const deltaTime = timestamp - lastFrameTime;
         fps = Math.round(1000 / deltaTime);
@@ -77,18 +84,22 @@ function update(timestamp) {
 
     context.clearRect(0, 0, board.width, board.height);
 
+    //Contrôles
     if (keys['q']) {
         arrowrot -= rotationspeed;
     } else if (keys['d']) {
         arrowrot += rotationspeed;
     }
-    globalrotation += 0.05;
+    // Draw + global rotation
+    globalrotation += 0.00;
     context.save()
     context.translate(center.x, center.y)
     context.rotate(globalrotation)
+    drawbg()
     drawArrow();
     drawhexagonal();
-    context.restore()
+    context.restore();
+
     requestAnimationFrame(update);
 }
 
@@ -96,14 +107,20 @@ function drawArrow() {
     context.save();
     //context.translate(center.x, center.y);
     context.rotate((arrowrot + Math.PI/2)*1);
+    context.filter = "saturate(4)"
     context.drawImage(arrowimg, -arrow.width / 2, -arrow.height / 2 -100, arrow.width, arrow.height);
     //context.translate(Math.cos(arrowrot)*100, Math.sin(arrowrot)*100)
     context.restore();
 }
 
 function drawhexagonal() {
-    context.save()
-    //context.translate(center.x, center.y)
+    //context.save();
     context.drawImage(formehexagonale.img, -formehexagonale.width /2, -formehexagonale.height/2, formehexagonale.width, formehexagonale.height);
-    context.restore();
+    //context.restore();
+}
+
+function drawbg() {
+    //context.save();
+    context.drawImage(bg.img, -bg.width/2, -bg.height/2, bg.width, bg.height);
+    //context.restore();
 }
